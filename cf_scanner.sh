2121f070f303fc5695e4b61ab80a6f6f12cacafa
Pourya <adeli.pouria@gmail.com>
2026-06-08 02:48:21 +0330

    added download test using curl for top 20

--- cf_scanner.sh
+++ cf_scanner.sh
@@ -4,17 +4,19 @@ INPUT_FILE="subnets.txt"
 
 OUTPUT_FILE="ipv4.txt"
 TOP_20_FILE="top20_ips.txt"
+FINAL_SPEED_FILE="best_ips_with_speed.txt"
 
 # clear old ones
 > "$OUTPUT_FILE"
 > "$TOP_20_FILE"
+> "$FINAL_SPEED_FILE"
 
 echo "Downloading the CIDRs..."
 curl -s "https://www.cloudflare.com/ips-v4/" -o "$INPUT_FILE"
 
 echo "Scanning 50 random IPs for each subnet (Super Fast!)..."
 
-while IFS= read -r subnet; do
+while read -r subnet; do
     if [[ -z "$subnet" ]]; then
         continue
     fi
@@ -32,4 +34,27 @@ done < "$INPUT_FILE"
 echo "The scan has been finished, sorting for the best ones..."
 sort -k2 -n "$OUTPUT_FILE" | head -n 20 > "$TOP_20_FILE"
 
-echo "Done! The top 20 IPs have been saved to $TOP_20_FILE"
+echo "Speed test for top 20 IPs"
+
+while read -r IP PING; do
+    echo -n "Testing $IP (Ping: ${PING}ms)...";
+
+    # 25 MB in 5 secs test
+    SPEED_BPS=$(curl -s --connect-timeout 2 --max-time 5 \
+        --resolve "speed.cloudflare.com:443:$IP" \
+        "https://speed.cloudflare.com/__down?bytes=25000000" \
+        -o /dev/null -w "%{speed_download}")
+
+    if [[ -z "$SPEED_BPS" || "$SPEED_BPS" == "0.000" ]]; then
+        echo "Failed"
+    else
+        SPEED_MB=$(echo "$SPEED_BPS" | awk '{printf "%.2f", $1/1048576}')
+        echo "$SPEED_MB MB/s"
+        echo "$IP $PING $SPEED_MB" >> "$FINAL_SPEED_FILE"
+    fi
+done < "$TOP_20_FILE"
+
+echo "Sorting final results by Download Speed..."
+sort -k3 -n -r "$FINAL_SPEED_FILE" > "final_top_ips.txt"
+
+echo "Done! The top 20 IPs have been saved to $FINAL_SPEED_FILE"
